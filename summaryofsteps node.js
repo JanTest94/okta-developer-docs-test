0458ac3249882296ef5a7f9d27f73d2cba7633cf
JanTest94 <jan.sreenivasan@gmail.com>
2024-11-18 21:03:36 -0500

    Create summaryofsteps node.js

---
+++ summaryofsteps node.js
@@ -0,0 +1,11 @@
+The following diagram summarizes all the steps involved in a password-only sign-in flow.
+
+<div class="full">
+
+![A sequence diagram showing the steps in a password-only sign-in flow](/img/oie-embedded-sdk/oie-embedded-nodejs-sign-in-pwd-only-flow-diagram.png)
+
+<!--
+   Source image: https://www.figma.com/file/YH5Zhzp66kGCglrXQUag2E/%F0%9F%93%8A-Updated-Diagrams-for-Dev-Docs?type=design&node-id=4640-82438&mode=design&t=euNZ4f92mOLnin29-11  oie-embedded-nodejs-sign-in-pwd-only-flow-diagram
+-->
+
+</div>

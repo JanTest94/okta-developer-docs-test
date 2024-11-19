bd5bc76462f18cf0c657605d0c4b48a64073e33e
JanTest94 <jan.sreenivasan@gmail.com>
2024-11-19 10:19:24 -0500

    Update integrationsteps node.js

--- integrationsteps node.js
+++ integrationsteps node.js
@@ -17,7 +17,7 @@ Source image: https://www.figma.com/file/YH5Zhzp66kGCglrXQUag2E/%F0%9F%93%8A-Upd
 
 ### The user submits their username and password
 
-When the user submits their `username` and `password`, pass them as parameters to [`OktaAuth.idx.authenticate()`](https://github.com/okta/okta-auth-js/blob/master/docs/idx.md#idxauthenticate).
+When the user submits their `username` and `password`, pass them as parameters to [`OktaAuth.idx.authenticate()`](https://github.com/okta/okta-auth-js/blob/master/docs/idx.md#idxauthenticate) to capture their login credentials.
 
 ```javascript
 const authClient = getAuthClient(req);
@@ -31,6 +31,8 @@ const transaction = await authClient.idx.authenticate({ username, password });
 #### Processing successful login
 
 After the user supplies their correct password, `IdxStatus` equals `IdxStatus.SUCCESS`. Call `tokenManager.setTokens()` to save the tokens retrieved for future requests and redirect the user back to the home page. The user is now signed in.
+
+ 
 
 ```js
   const { nextStep, tokens, status, error, } = transaction;
@@ -51,7 +53,9 @@ After the user supplies their correct password, `IdxStatus` equals `IdxStatus.SU
 
 #### Handling other authentication statuses
 
-The app must handle other `IdxStatus` cases if the user didn't successfully sign-in or if additional validation is required. Below is an example: 
+The app must handle other `IdxStatus` values if the user didn't successfully sign-in or if additional validation is required.
+ 
+ See the below process flow for each returned `IdxStatus` value captured in the relevant case statements:
 
 ```js
 switch (status) {
